13aaafaad5514e046c57c21ba93f89a14ba0874c
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2019-11-30 17:40:56 +0700

    fail

--- essay_file/essay.py
+++ essay_file/essay.py
@@ -1,6 +1,9 @@
 def main():
     Read_line()
     Read_Sentense()
+    Read_Word()
+    Read_character()
+    Char_upper()
 def Read_line():
     infile = open("/Users/again/Desktop/GIT/ProblemSolving/essay_file/Loop.txt",'r')
     readline = infile.read()
@@ -17,6 +20,24 @@ def Read_Sentense():
     readsentense = infile.read()
     infile.close()
     read_splitdot = readsentense.split(".")
-    print(read_splitdot)
+    print(len(read_splitdot))
+def Read_Word():
+    infile = open("/Users/again/Desktop/GIT/ProblemSolving/essay_file/Loop.txt",'r')
+    readword = infile.read()
+    infile.close()
+    read_wordsplit = readword.split()
+    print(len(read_wordsplit))
+def Read_character():
+    infile = open("/Users/again/Desktop/GIT/ProblemSolving/essay_file/Loop.txt",'r')
+    readcharacter = infile.read()
+    infile.close()
+    read_charlen = len(readcharacter)
+    print(read_charlen)
+def Char_upper():
+    infile = open("/Users/again/Desktop/GIT/ProblemSolving/essay_file/Loop.txt",'r')
+    read_upper = infile.read()
+    infile.close()
+    char_up = read_upper.isupper()
+    print(len(char_up))
 main()
             
