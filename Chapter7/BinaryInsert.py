4ea860d472346d20161aff4c3c82740fb77a90f1
ohmtanawin02 <againnakub@hotmail.com>
2020-01-28 13:53:56 +0700

    commit

--- Chapter7/BinaryInsert.py
+++ Chapter7/BinaryInsert.py
@@ -29,6 +29,18 @@ class Node:
         if self.right:
             self.right.PrintTree()
 
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+" Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+" Not Found"
+            return self.right.findval(lkpval)
+        else:
+            print(str(self.data) + " is found")
+
 
 root = Node(60)
 root.insert(65)
