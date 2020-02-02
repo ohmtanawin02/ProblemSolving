9101a82961e16aaeb8605996e5fb23eccba1eb5a
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2020-02-02 19:36:09 +0700

    lol

--- Chapter7/BinaryInsert.py
+++ Chapter7/BinaryInsert.py
@@ -1,13 +1,10 @@
 class Node:
-
-    def __init__(self, data):
-
+    def __init__(self,data):
         self.left = None
         self.right = None
         self.data = data
-
-    def insert(self, data):
-
+    
+    def insert(self,data):
         if self.data:
             if data < self.data:
                 if self.left is None:
@@ -21,81 +18,84 @@ class Node:
                     self.right.insert(data)
         else:
             self.data = data
-
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
         print(self.data),
         if self.right:
             self.right.PrintTree()
-
-    def findval(self, data):
-        if data < self.data:
+    def findval(self,lkpval):
+        if lkpval < self.data:
             if self.left is None:
-                return str(data)+" Not Found"
-            return self.left.findval(data)
-        elif data > self.data:
+                return str(lkpval)+"Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
             if self.right is None:
-                return str(data)+" Not Found"
-            return self.right.findval(data)
+                return str(lkpval)+"Not Found"
+            return self.right.findval(lkpval)
         else:
-            print(str(self.data) + " is found")
-
-    def PostorderTraversal(self, root):
-        res = []
-        if root:
-            res = self.PostorderTraversal(root.left)
-            res = res + self.PostorderTraversal(root.right)
-            res.append(root.data)
-        return res
-
-    def PreorderTraversal(self, root):
+            print(str(self.data)+" is Found")
+    def inorderTraversal(self,root):
+            res = []
+            if root:
+                res = self.inorderTraversal(root.left)
+                res.append(root.data)
+                res = res + self.inorderTraversal(root.right)
+            return res
+    def PreorderTraversal(self,root):
         res = []
         if root:
             res.append(root.data)
             res = res + self.PreorderTraversal(root.left)
             res = res + self.PreorderTraversal(root.right)
         return res
-
-    def inorderTraversal(self, root):
+    def PostorderTrraversal(self,root):
         res = []
         if root:
-            res = self.inorderTraversal(root.left)
+            res = self.PostorderTrraversal(root.left)
+            res = res+self.PostorderTrraversal(root.right)
             res.append(root.data)
-            res = res + self.inorderTraversal(root.right)
         return res
-
-    def DeleteBinary(self, root):
-        if self.root is None:
-            if root < self.root:
-                if self.left is None:
-                    self.left = DeleteBinary(self.left, root)
-            elif root > self.root:
-                if self.right is None:
-                    self.right = DeleteBinary(self.left, root)
+    def goinNode(self, node):
+        deepgo = node
+        while(deepgo.left is not None):
+            deepgo = deepgo.left
+        return deepgo
+    def delete(self, data):
+        if self is None:
+            return None 
+        if data < self.data:
+            self.left = self.left.delete(data)
+        elif data > self.data:
+            self.right = self.right.delete(data)
         else:
-            self.root = data
+            if self.left is None:
+                dataX = self.right
+                self = None
+                return dataX
+            elif self.right is None:
+                dataX = self.left
+                self = None
+                print(dataX)
+                return dataX
+            dataX = self.goinNode(self.right)
+            self.data = dataX.data
+            self.right = self.right.delete(dataX.data)
 
-
-root = Node(60)
-root.insert(65)
-root.insert(75)
-root.insert(32)
-root.insert(47)
+        return self
+    
+root = Node(100)
 root.insert(50)
-root.insert(81)
-root.insert(51)
-root.insert(73)
-root.findval(65)
-
+root.insert(60)
+root.insert(30)
+root.insert(150)
+root.insert(120)
+root.delete(150)
 print(root.inorderTraversal(root))
-print(root.PreorderTraversal(root))
-print(root.PostorderTraversal(root))
-print(root.DeleteBinary(51))
+#print(root.PreorderTraversal(root))
+#print(root.PostorderTrraversal(root))
+#for i in range(51):
+    #root.insert(i)
+root.PrintTree()
+#print(root.findval(56))
 
-# if root < self.data:
-#self.left = DeleteBinary(self.left, root)
-# elif root > self.data:
-#self.right = DeleteBinary(self.right, root)
-# else:
-#print('dont delete')
