9687b1375375d66e08d96161db1e3b90e26c8259
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2020-02-11 21:12:06 +0700

    update

--- Final/ex2.py
+++ Final/ex2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import string
 
 G = nx.Graph()
 
@@ -10,6 +11,7 @@ G.add_edges_from([('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'K'), ('B', 'G'),
                   ('J', 'F'), ('J', 'H'), ('J', 'Z'), ('Z', 'E'), ('Z', 'J'),
                   ('B', 'A')])
 add = input("Input :")
+
 result = []
 for i in G.neighbors(add):
     for n in G.neighbors(i):
@@ -17,6 +19,7 @@ for i in G.neighbors(add):
             del n
         else:
             print(n)
+
 
 nx.draw(G, with_labels=True, font_color='yellow', node_size=1500)
 plt.show()
--- Final/ex3.py
+++
@@ -1,5 +0,0 @@
-ListA = ['A','B','C','D','E','F']
-
-
-first = input('Input first : ')
-ListA.pop(first)
